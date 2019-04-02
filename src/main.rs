6ff5976d2f9abe14e3e2252fd65f3952664083ea
itaton <stefan.gapp@t-online.de>
2019-04-02 16:52:37 +0200

    first idea for showing ships

--- src/main.rs
+++ src/main.rs
@@ -66,15 +66,17 @@ fn main() -> ! {
     let blue = Color{red: 0,green: 0 ,blue: 255,alpha: 255};
     let green = Color{red: 0,green: 255 ,blue: 0,alpha: 255};
     let black = Color{red: 0,green: 0 ,blue: 0,alpha: 255};
+    let grey = Color{red: 127,green: 127 ,blue: 127,alpha: 127};
     layer_1.clear();
     layer_2.clear();
     lcd.set_background_color(blue);
 
-    let arr = [24,48,72,96,120,144,168,192,216,240,264];
+    let arr = [24,25,49,50,74,75,99,100,124,125,149,150,174,175,199,200,224,225,249,250,274,275];
     //let arr = [200,210,220];
     //let arr = [10,20,30];
-    let arr2 = [24,48,72,96,120,144,168,192,216,240,264];
-    
+    let arr2 = [24,25,49,50,74,75,99,100,124,125,149,150,174,175,199,200,224,225,249,250];
+    //let arr2 = [24,48,72,96,120,144,168,192,216,240,264];
+    //let ship1 = []
 
     // turn led on
     pins.led.set(true);
@@ -102,8 +104,13 @@ fn main() -> ! {
             }
         }
         for c in arr2.iter() {
-            for i in 0..480 {
+            for i in 0..275 {
                 layer_1.print_point_color_at(i, *c, black);
+            }
+        }
+        for c in 53..72 {
+            for i in 78..172 {
+                layer_2.print_point_color_at(c, i, grey);
             }
         }
         //layer_1.clear();
