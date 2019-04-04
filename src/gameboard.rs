bca4ba0220311966ca9d699d474ad9d4f4562b62
itaton <stefan.gapp@t-online.de>
2019-04-04 17:00:09 +0200

    started implementing the gameboard class - still a lot of errors

--- src/gameboard.rs
+++ src/gameboard.rs
@@ -1,9 +1,23 @@
 
+use crate::ships::Ship;
+use crate::display::Display;
+
 struct Board {
-    game_field:[[Block];Block],
-    ships:[Ship],
-    fields_shot:[[bool]bool],
+    game_field:[[Block; 10];10],
+    ships:[Ship; 5],
+    fields_shot:[[bool; 10];10],
     display: Display,
+}
+
+impl Board {
+    pub fn new(game_field: [[Block; 10];10], ships: [Ship; 5], fields_shot:[[bool; 10];10], display: Display) -> Board {
+        Board {
+            game_field,
+            ships,
+            fields_shot,
+            display,
+        }
+    }
 }
 
 pub struct Block {
@@ -11,7 +25,36 @@ pub struct Block {
     y: u8,
 }
 
-pub fn gameboard_init() -> Board {
+pub fn gameboard_init(display: Display) -> Board {
+    //let mut ships : [Ship] = [];
+    display.setup_ship(5); //only display right side here
+    //let ship: [Block; 5] = input_x();
+    //check_valid_ship(ship, ships);
+    //ships.push_back(ship);
+    display.setup_ship(4);
+    //let ship: [Block; 4] = input_x();
+    //check_valid_ship(ship, ships);
+    //ships.push_back(ship);
+    display.setup_ship(3);
+    //let ship: [Block; 3] = input_x();
+    //check_valid_ship(ship, ships);
+    //ships.push_back(ship);
+    display.setup_ship(3);
+    //let ship: [Block; 3] = input_x();
+    //check_valid_ship(ship, ships);
+    //ships.push_back(ship);
+    display.setup_ship(2);
+    //let ship: [Block; 2] = input_x();
+    //check_valid_ship(ship, ships);
+    //ships.push_back(ship);
+
+    let fields_shot = [[false; 10];10];
+
+    let game_field = //TODO initialize with the blocks
+
+    board = Board::new(game_field, ships, fields_shot, display);
+    return board;
+
     
 }
 
@@ -19,7 +62,7 @@ pub fn calculate_touch_block(x: u16, y: u16) -> Block {
     if x<=272 && x>24 && y <= 272 && y > 24 {
         let x_block = x/25;
         let y_block = y/25;
-        (x_block,y_block) //change to Block
+        (x_block,y_block) //TODO: change to Block
     } else {
         (0,0)
     }
--- src/ships.rs
+++ src/ships.rs
@@ -11,12 +11,12 @@ enum Direction {
     horizontal
 }
 
-pub impl Ship {
-    fn new(new_size: u8, x_location: u8, y_location: u8) -> Ship {
-        Ship {
-            size: new_size,
-            x_start_location: x_location,
-            y_start_location: y_location,
-        }
-    }
-}
+//pub impl Ship {
+//    fn new(new_size: u8, x_location: u8, y_location: u8) -> Ship {
+//        Ship {
+//            size: new_size,
+//            x_start_location: x_location,
+//            y_start_location: y_location,
+//        }
+//    }
+//}
