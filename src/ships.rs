179ecf8a3eb5b7eef2ff77fae782cbd78edd8065
Joachim Müssig <uneid@student.kit.edu>
2019-04-04 10:31:43 +0200

    add direction

--- src/ships.rs
+++ src/ships.rs
@@ -1,16 +1,22 @@
 
-struct Ship {
-    size: i8;
-    x_start_location: i8;
-    y_start_location: i8;
+pub struct Ship {
+    size: u8,
+    x_start_location: u8,
+    y_start_location: u8,
+    direction: Direction,
+}
+//enum for direction ?
+enum Direction {
+    vertical,
+    horizontal
 }
 
-impl Ship {
-    fn new(new_size: i8, x_location: i8, y_location: i8) -> Ship {
+pub impl Ship {
+    fn new(new_size: u8, x_location: u8, y_location: u8) -> Ship {
         Ship {
             size: new_size,
-            x_start_location = x_location,
-            y_start_location = y_location,
+            x_start_location: x_location,
+            y_start_location: y_location,
         }
     }
 }
