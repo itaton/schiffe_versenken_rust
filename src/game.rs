3f924ce1925df26ac10e18bdc3bc48feb19809eb
Joachim Müssig <uneid@student.kit.edu>
2019-04-08 15:48:14 +0200

    use display references for board calls

--- src/game.rs
+++ src/game.rs
@@ -13,12 +13,12 @@ use crate::display::{
     Display
 };
 
-struct Game<'a> {
+pub struct Game {
     game_state: Gamestate,
-    board: Board,
-    display: Display,
-    network: Network<'a>,
-    ethernet_c: EthClient,
+    pub display: Display,
+    pub board: Board,
+    // network: Network<'a>,
+    pub ethernet_c: EthClient,
 }
 
 #[derive(PartialEq, Eq)]
@@ -31,27 +31,26 @@ enum Gamestate {
 }
 
 //start game, init field and wait for other player
-pub fn init_new_game<'a>(display: Display,is_server: bool) -> Game<'a> {
+pub fn init_new_game(display: Display,is_server: bool) -> Game {
     Game::new(display, is_server)    
 }
 
 
 
 
-//game loop
-impl<'a> Game<'a> {
-    fn new(display: Display, is_server: bool) -> Game<'a> {
+impl Game {
+    fn new(display: Display, is_server: bool) -> Game {
         Game {
             game_state: Gamestate::GameStart,
             // board: Board::new(), //TODO: without params ? gameboard creates the start state ? 
-            board: gameboard::gameboard_init(display),
             display,
-            network: network.new(),
+            board: gameboard::gameboard_init(),
+            // network: network::init(rcc: &mut RCC, syscfg: &mut SYSCFG, ethernet_mac: &mut ETHERNET_MAC, ethernet_dma: &'a mut ETHERNET_DMA, is_server: bool),
             ethernet_c: EthClient::new(is_server),
         }
     }
 
-    pub fn run_game(&self) {
+    pub fn run_game(&mut self) {
         loop {
             match self.game_state {
                 Gamestate::YourTurn => self.select_shoot_location(),
@@ -63,7 +62,7 @@ impl<'a> Game<'a> {
         }
     }
 
-    fn set_game_state(&self, state: Gamestate) {
+    fn set_game_state(&mut self, state: Gamestate) {
         match state {
             Gamestate::YourTurn => {
                 assert!(self.game_state == Gamestate::WaitForEnemy);
@@ -87,58 +86,61 @@ impl<'a> Game<'a> {
         }
     }
 
-    fn show_start_screen(&self) {
+    fn show_start_screen(&mut self) {
         self.display.show_start_screen();
         loop {
             let (x,y) = self.display.touch();
-            if !((x,y) == (0,0)) {
+            if (x,y) != (0,0) {
                 self.set_game_state(Gamestate::SetupShips);
             }
         }
-   }
+    }
 
-    fn show_win_screen(&self) {
+    fn show_win_screen(&mut self) {
         //Show win screen
         self.display.show_win_screen();
     }
 
     //TODO: check if gameboard and network is implemented
-    fn wait_and_check_enemy_shot(&self) {
-        //recvn enemy shot packet and check hit 
-        let enemy_shot = self.ethernet_c.recv_shoot(self.ethernet_c, self.network);
-        //now check hit
-        let (hit, sunk) = self.board.shot_at(Block {x: enemy_shot.column, y: enemy_shot.line});
-        let mut ship_sunk_size = 0;
-        if sunk {
-            //get ship size 
-            // ship_sunk = 
-        }
-        //create feedback packet
-        let win = self.board.check_win();
-        let feedback = packets::FeedbackPacket::new(hit, ship_sunk_size, win);
-        self.ethernet_c.send_feedback(self.network, feedback);
-        self.set_game_state(Gamestate::YourTurn);
+    fn wait_and_check_enemy_shot(&mut self) {
+        // //recvn enemy shot packet and check hit 
+        // // let enemy_shot = self.ethernet_c.recv_shoot(self.ethernet_c, self.network);
+        // //now check hit
+        // let (hit, sunk) = self.board.shot_at(Block {x: enemy_shot.column, y: enemy_shot.line});
+        // let mut ship_sunk_size = 0;
+        // if sunk {
+        //     //get ship size 
+        //     // ship_sunk = 
+        // }
+        // //create feedback packet
+        // let win = self.board.check_win();
+        // let feedback = packets::FeedbackPacket::new(hit, ship_sunk_size, win);
+        // self.ethernet_c.send_feedback(self.network, feedback);
+        // self.set_game_state(Gamestate::YourTurn);
     }
 
     //send shoot packet and check hit
-    fn fire(&self, block: Block) {
-        let shoot_packet = packets::ShootPacket::new(block.y, block.x); //TODO set x,y public
-        //use network file and send package
-        self.ethernet_c.send_shoot(self.network, shoot_packet); 
-        //wait for answer
-        let feedback_packet = self.ethernet_c.recv_feedback(self.network);
-        if feedback_packet.you_win = true {
-            self.set_game_state(Gamestate::Won);
-        } else if feedback_packet.hit {
-            let sunk_size = feedback_packet.sunk;
-            //TODO: set red cross on display 
-        } else {
-            //TODO: set white cross and set corresponding field as shot in gameboard
-        }
+    fn fire(&mut self, block: Block) {
+
+        // let shoot_packet = packets::ShootPacket::new(block.y, block.x); //TODO set x,y public
+        // //use network file and send package
+        // self.ethernet_c.send_shoot(self.network, shoot_packet); 
+        // //wait for answer
+        // let feedback_packet = self.ethernet_c.recv_feedback(self.network);
+        // if feedback_packet.you_win = true {
+        //     self.set_game_state(Gamestate::Won);
+        // } else if feedback_packet.hit {
+        //     self.display.write_in_field_layer2(block.x, block.y, "o");
+        //     let sunk_size = feedback_packet.sunk;
+        // } else {
+        //     self.display.write_in_field_layer2(block.x, block.y, "x");
+        // }
+        // //clear all x on layer_1
+        // self.board.clear_x_es(&self.display);
     }
     
-    fn setup_ships(&self) {
-        self.board.initial_setup();
+    fn setup_ships(&mut self) {
+        self.board.initial_setup(&mut self.display);
         //TODO: send ready packet and wait for other players ready packet
 
         if self.ethernet_c.is_server {
@@ -148,10 +150,10 @@ impl<'a> Game<'a> {
         }
     }
 
-    fn select_shoot_location(&self) {
-        let confirmed = false;
-        let block_set = false;
-        let mut block;
+    fn select_shoot_location(&mut self) {
+        let mut confirmed = false;
+        let mut block_set = false;
+        let mut block = Block{x: 0, y: 0};
         //create methods in display to handle touch
         while !confirmed {
             let (x,y) = self.display.touch();
@@ -166,7 +168,7 @@ impl<'a> Game<'a> {
                 Some(ret_block) => {
                     //delete old block and set new
                     if (block_set) {
-                       self.board.clear_x_es(); 
+                       self.board.clear_x_es(&mut self.display); 
                     }
                     self.display.write_in_field(ret_block.x as usize, ret_block.y as usize, "x");
                     block = ret_block;
