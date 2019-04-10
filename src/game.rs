3b29809f6ef1b85bad0b66cd8362a8b1687f8036
Joachim Müssig <uneid@student.kit.edu>
2019-04-10 18:06:30 +0200

    set enemy_ship_hit

--- src/game.rs
+++ src/game.rs
@@ -159,6 +159,7 @@ impl Game {
             return;
         } else if feedback_packet.hit {
             self.display.write_in_field(block.x as usize, block.y as usize, "X");
+            self.display.enemy_ships_hit[block.x][block.y] = true;
             let sunk_size = feedback_packet.sunk;
             if feedback_packet.sunk != 0 {
                 self.display.clear_text_on_display();
